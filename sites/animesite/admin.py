5763ca23be8a21c21945430b622966ab42758d64
tema-glumskov <58003716+tema-glumskov@users.noreply.github.com>
2024-07-24 16:12:22 +0300

    Add files via upload

--- sites/animesite/admin.py
+++ sites/animesite/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import AnimeTitle
+from .models import AnimeTitle, AnimeUser
 
 # Register your models here.
 admin.site.register(AnimeTitle)
+admin.site.register(AnimeUser)
--- sites/animesite/views.py
+++ sites/animesite/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
-from animesite.models import AnimeTitle
+from django.contrib import messages
+from animesite.models import AnimeTitle, AnimeUser
 
 # переменные сайта
 locked = True
@@ -7,6 +8,7 @@ locked = True
 # Create your views here.
 def index(request):
     global locked
+    global login
     context = {'name': 'Артём'}
     if request.method == 'POST':
         login = request.POST.get('username')
@@ -14,12 +16,22 @@ def index(request):
         if login == 'admin' and password == '1234':
             locked = False
             return redirect('anime')
+        elif AnimeUser.objects.filter(login=login, password=password).exists():
+            locked = False
+            return redirect('anime')
     return render(request, 'index.html', context=context)
 
 def anime(request):
     global locked
+    global login
     context = {'anime_titles': AnimeTitle.objects.all()}
-
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        user = AnimeUser.objects.filter(login=login).first()
+        user.favourite_anime.append(title)
+        user.save()
+        print(user.favourite_anime)
+        print('Угабуга!')
     if locked:
         return redirect('https://youtu.be/dQw4w9WgXcQ?si=N5_ZSmfxseh59c3k')
     return render(request, 'anime.html', context=context)
@@ -28,4 +40,12 @@ def manga(request):
     return render(request, 'manga.html')
 
 def register(request):
+    if request.method == 'POST':
+        login = request.POST.get('username')
+        password = request.POST.get('password')
+        if not(AnimeUser.objects.filter(login=login).exists()):
+            AnimeUser.objects.create(login=login, password=password)
+            messages.success(request, 'Регистрация прошла успешно!')
+        else:
+            messages.success(request, 'Такой пользователь уже существует!')
     return render(request, 'register.html')
